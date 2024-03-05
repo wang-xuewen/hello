e19813b14239506ba42f0c7d13e17ef0263646c5
wang-xuewen <40913788@qq.com>
2024-03-05 16:45:26 +0800

    add 404.html

--- src/main.rs
+++ src/main.rs
@@ -26,5 +26,12 @@ fn handle_connection(mut stream: TcpStream) {
         stream.flush().unwrap();
     } else {
         // 一些其他的请求
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let contents = fs::read_to_string("404.html").unwrap();
+
+        let response = format!("{}{}", status_line, contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
     }
 }
