147af7d0332b8b4ee6767f650012013559c1d104
wang-xuewen <40913788@qq.com>
2024-03-05 15:28:52 +0800

    add response html

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
+use std::fs;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("172.16.5.62:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -14,5 +15,11 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let contents = fs::read_to_string("hello.html").unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
